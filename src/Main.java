35d448fcd2a38d11c45bf57bf1eb647f50d02a73
Danil <123>
2023-04-21 15:24:47 +0300

    фикс

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите сколько необходимо проехать [км]: ");
-        CarThread CarThread = new CarThread("CarThread",sc.nextInt());
-        NavThread thread = new NavThread("NavThread", CarThread.time, CarThread.currentDistance, CarThread.distance, CarThread.speed, CarThread);
+        CarThread CarThread = new CarThread("CarThread");
+        NavThread thread = new NavThread("NavThread", CarThread, sc.nextFloat());
+        CarThread.navigator = thread;
         thread.start();
-        System.out.println("Ваш путь : " + CarThread.distance + "[км]");
         CarThread.start();
-
     }
 }
